c1a3dea9a2a0d497776b74780856d72cef66d449
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-08-31 21:08:52 +0800

    Set up initial file

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,44 +1,21 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer } from 'graphql-yoga';
 
-const typeDefs =
-`
-  type Query {
-    hello(pangalan: String): User!
-    add(a: Int, b: Int): Int!
-    substract(numbers: [Int!]!): Int!
-  }
+import typeDefs from './typeDefinitions';
+import resolvers from './resolvers/index';
+import { users, comments, posts } from './mockData';
 
-  type User {
-    name: String!
-    age: Int!
-    job: String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: (_, { pangalan }) => (
-      {
-        name: `Hello ${pangalan || 'World'}!`,
-        age: pangalan ? 26 : null,
-        job: pangalan ? 'Software Engineer' : null
-      }
-    ),
-    // hello(parent, args, ctx, info) {
-    //   return {
-    //     name: `Hello ${args.pangalan || 'World'}`,
-    //     age: args.pangalan ? 26 : null,
-    //     job: args.pangalan ? 'Software Engineer' : null
-    //   }
-    // },
-    add: (_, { a, b }) => a + b,
-    substract: (_, { numbers }) => numbers.reduce((accumulator, currentValue) => accumulator - currentValue)
-  },
-}
+const context = () => {
+  return {
+    users,
+    comments,
+    posts
+  };
+};
 
 const server = new GraphQLServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+  context
+});
 
 server.start(() => console.log('Server is running on localhost:4000'))
