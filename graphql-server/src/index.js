20901d50affd6e73d2bb1208da5d426423d2e392
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-09-15 22:00:41 +0800

    Implement PubSub

--- graphql-server/src/index.js
+++ graphql-server/src/index.js
@@ -1,12 +1,16 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 import resolvers from './resolvers';
 import db from './db';
 
+const pubsub = new PubSub();
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: db
+  context: {
+    ...db,
+    pubsub
+  }
 });
 
 server.start(() => console.log('Server is running on localhost:4000'));
