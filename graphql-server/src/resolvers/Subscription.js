6fbe38e1bddbd32224e4c4bfd7a5672c9adcd3fe
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-09-15 22:01:09 +0800

    Create a comment subscription

---
+++ graphql-server/src/resolvers/Subscription.js
@@ -0,0 +1,13 @@
+const Subscription = {
+  comment: {
+    subscribe(parent, { postId }, { posts, pubsub }, info) {
+      const post = posts.find(post => (post.id === postId) && post.published);
+
+      if (!post) throw new Error('Post not found');
+
+      return pubsub.asyncIterator(`comment ${postId}`)
+    }
+  }
+};
+
+export { Subscription as default };
