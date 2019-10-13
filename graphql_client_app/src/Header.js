169cb25f529d9e090f0d90d8af7b37bb879de7af
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-10-13 20:54:27 +0800

    Create Header

---
+++ graphql_client_app/src/Header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from 'react-native';
+
+import Portrait from './Portrait';
+
+const Header = () => {
+  const placeholder = 'Tell us your recent Booky experience!';
+  return (
+    <View style={styles.container}>
+      <Portrait style={styles.portrait} />
+      <View style={styles.headerContent}>
+        <Text style={styles.currentUser}>Juan de la Cruz</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder={placeholder}
+          placeholderTextColor={'#aeaeae'}
+          textAlign={'center'}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F58855',
+    height: 80,
+    padding: 20,
+    flexDirection: 'row',
+  },
+  portrait: {
+    paddingRight: 20,
+  },
+  headerContent: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  currentUser: {
+    color: '#222222',
+    fontSize: 16,
+    fontWeight: '800',
+  },
+  textInput: {
+    width: 240,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: '#ffffff',
+    fontSize: 12,
+  },
+});
+
+export default Header;
