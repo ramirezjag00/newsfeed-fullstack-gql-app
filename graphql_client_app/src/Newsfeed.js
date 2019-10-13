540882eff6a98b56ff7ba61f4d0ebb2d5bea2cbd
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-10-13 20:55:05 +0800

    Implement Post Component

--- graphql_client_app/src/Newsfeed.js
+++ graphql_client_app/src/Newsfeed.js
@@ -1,45 +1,53 @@
-import React from 'react';
-import { View, Text, Flatlist } from 'react-native';
+import React, { Fragment, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 
+import Posts from './Posts';
+
 const GET_POSTS = gql`
-  query Post(
-    $queryContent: String,
-    $id: String,
-    ) {
-    posts(queryContent: $queryContent, id: $id) {
+  query {
+    posts {
       id
-      title
       body
-      published
       author {
-        id
         name
-        email
       }
       comments {
         id
-        text
       }
     }
   }
 `;
 
 const Newsfeed = () => {
-const { loading, error, data } = useQuery(GET_POSTS);
-if (loading) return null;
-if (error) console.log(error);
-if (!loading && !error) {
-  console.log(data)
-}
+  const [posts, setPosts] = useState([]);
+  const { loading, error, data } = useQuery(GET_POSTS);
+  if (loading) return <Text style={styles.loading}>Loading ...</Text>;
+  if (error) console.log(error);
+  if (!loading && !error && data.posts !== posts) setPosts(data.posts);
   return (
-    <View>
-      <Text>
-        Hello
-      </Text>
+    <View style={styles.container}>
+      <Posts items={posts} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  loading: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'red',
+    fontSize: 20,
+  },
+});
 
 export default Newsfeed;
